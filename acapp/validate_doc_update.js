c7bd1086763f5f8d2de289d4d258a90a3a51566f
Ryan Yeske <rcyeske@gmail.com>
2010-09-05 14:26:48 -0700

    add validation for control messages

--- acapp/validate_doc_update.js
+++ acapp/validate_doc_update.js
@@ -1,15 +1,17 @@
-function(newDoc, oldDoc, userCtx) 
-{
-  function require(field, message) 
-  {
+function(newDoc, oldDoc, userCtx) {
+  function require(field, message) {
     message = message || "Document must have a " + field;
     if (!newDoc[field]) throw({forbidden : message});
   };
 
   require('type');
 
-  if (newDoc.type == 'message') 
-  {
+  if (newDoc.type == 'message') {
     require('cmd');
+    require('sender');
+    require('target');
+  } else if (newDoc.type == 'control') {
+    require('cmd');
+    require('target');
   }
 }
