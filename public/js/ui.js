a6daa2d26abd9f2303e06d6366cc93f38b96b812
Ryan Yeske <rcyeske@gmail.com>
2010-09-15 21:29:55 -0700

    show users color in input bar

--- public/js/ui.js
+++ public/js/ui.js
@@ -3,11 +3,12 @@ var ui = {
 
   template: {
     motd: '<div class="server motd"><h1>{{head}}</h1><p>{{body}}</p>{{{button}}}',
-    privmsg: '<div class="privmsg {{color}}">{{nick}}: {{body}}</div>',
+    privmsg: '<div class="privmsg"><span class="{{color}}">{{nick}}:</span> {{body}}</div>',
     waiting: '<div class="server waiting">{{body}}</div>',
     init: '<div class="server"><p>Your color is <span class="{{color}}">{{color}}</span></p><p>You are playing against {{#opponents}}<span class="{{.}}">{{.}}</span> {{/opponents}}</p>The game will last {{seconds}} seconds.</p><br /></div>',
     wait: '<p class="server">{{reason}}</p>',
     go: '<p class="server">GO!</p>',
+
     vote: '<div class="server vote"><h2>GAME OVER</h2><p>You have {{seconds}} seconds finalize your pick.</p></div>',
 
     // results
@@ -72,10 +73,11 @@ var ui = {
                                color: o});
     });
     var html = 'pick: '+hs.join(' ');
-    $("#status .vote").html(html);
+    //$("#status .vote").html(html);
   },
   showSelf: function(obj) {
-    $("#status .self").html(Mustache.to_html('<span class="self {{color}}">{{name}} is {{color}}</span>', obj));
+    $("form.chat input").addClass(obj.color);
+    //$("#status .self").html(Mustache.to_html('<span class="self {{color}}">{{name}} is {{color}}</span>', obj));
   }
 };
 
