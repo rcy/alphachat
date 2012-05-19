065a126264dfcf97f2c97b086b4f0d21a126839e
Ryan Yeske <rcyeske@gmail.com>
2012-05-19 14:15:25 -0700

    add socket connectinos

--- express/app.js
+++ express/app.js
@@ -36,34 +36,21 @@ games.create({name:'alphachat classic',num_players:3}, function(){});
 
 // Routes
 app.get('/', function(req,res) {
+  res.redirect('/games');
+});
+
+app.get('/games', function(req,res) {
   games.all(function(error, docs) {
     res.render('gamelist', { title:'Game List', games:docs })
   })
 });
 
-app.post('/', function(req,res,data) {
+app.post('/games', function(req,res,data) {
   games.create({ name: req.param('name'),
                  num_players: req.param('num_players') },
                function(error, docs) {
                  res.redirect('/');
                });
-});
-
-app.post('/join/:id', function(req,res,data) {
-  games.find_by_id(req.params.id, function(error, doc) {
-    if (error) {
-      res.send("error: " + error.text);
-    } else {
-      if (doc) {
-        doc.players.push('me');
-        games.save(doc, function() {
-          res.redirect('/game/'+req.params.id);
-        });
-      } else {
-        res.send('not found');
-      }
-    }
-  });
 });
 
 app.get('/game/:id', function(req, res) {
@@ -82,4 +69,15 @@ app.get('/game/:id', function(req, res) {
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function(socket) {
+  console.log('connection!');
+  socket.on('disconnect', function() {
+    console.log('disconnect');
+  });
+  socket.on('join', function(data) {
+    console.log('join',data);
+  });
 });
